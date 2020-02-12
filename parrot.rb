99f74346c7a5efc61b5bb86f48d255a85aae38da
kyle molidor <k_molidor@yahoo.com>
2020-02-12 07:36:26 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def print_parrot ("Pretty Bird.")
+def print_parrot ("Pretty Bird!")
   name = "Squawk!"
   puts name
   return parrot
